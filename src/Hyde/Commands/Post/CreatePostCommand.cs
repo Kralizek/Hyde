f29a360b4971e3994707e2017a12af6de655bad6
Renato Golia <renato.golia@insightarchitectures.com>
2023-02-21 12:23:34 +0100

    Implemented create post command handler

--- src/Hyde/Commands/Post/CreatePostCommand.cs
+++ src/Hyde/Commands/Post/CreatePostCommand.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel;
+using System.Text;
+using System.Text.RegularExpressions;
 using Hyde.Utilities;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using YamlDotNet.Serialization;
 
 namespace Hyde.Commands.Post;
 
 public class CreatePostCommand : AsyncCommand<CreatePostCommand.CreatePostSettings>
 {
+    private readonly ISerializer _serializer;
+
     public class CreatePostSettings : PostSettings
     {
         [CommandArgument(0, "<TITLE>")]
@@ -38,22 +43,121 @@ public class CreatePostCommand : AsyncCommand<CreatePostCommand.CreatePostSettin
         public bool IsDraft { get; init; }
     }
 
-    public override Task<int> ExecuteAsync(CommandContext context, CreatePostSettings settings)
+    public CreatePostCommand(ISerializer serializer)
+    {
+        _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+    }
+
+    public override ValidationResult Validate(CommandContext context, CreatePostSettings settings)
+    {
+        if (!settings.IsInJekyll)
+        {
+            return ValidationResult.Error("This command can only be executed while in a Jekyll site");
+        }
+        
+        return ValidationResult.Success();
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, CreatePostSettings settings)
     {
         SettingsDumper.Dump(settings);
 
-        var filename = Path.Combine(settings.SiteDirectory.FullName, settings.IsDraft ? "_drafts" : "_posts", GetFileName(settings.PostDate, settings.Title));
+        var filename = new FileInfo(Path.Combine(settings.SiteDirectory.FullName, settings.IsDraft ? JekyllFolders.Drafts : JekyllFolders.Posts, GetFileName(settings)));
+
+        var frontMatter = CreateFrontMatterHeader(settings, context.Remaining.Parsed);
+
+        await AnsiConsole.Status().StartAsync($"Creating post '{settings.Title}'", async _ =>
+        {
+            if (!filename.Directory?.Exists ?? false)
+            {
+                AnsiConsole.WriteLine($"Directory '{filename.Directory.Name}' is missing. Creating it.");
+                
+                filename.Directory.Create();
+            }
+            
+            await using var sw = File.CreateText(filename.FullName);
+            
+            await sw.WriteLineAsync("---");
         
-        AnsiConsole.WriteLine($"Writing file {filename}");
+            var frontMatterText = _serializer.Serialize(frontMatter);
+
+            await sw.WriteAsync(frontMatterText);
+
+            await sw.WriteLineAsync("---");
+
+            await sw.WriteLineAsync();
+
+            await sw.WriteLineAsync("<!-- Post created by Hyde -->");
+        });
         
-        return Task.FromResult(0);
+        AnsiConsole.WriteLine($"{(!settings.IsDraft ? "Post" : "Draft for post")} '{settings.Title}' successfully created as '{filename.Name}'.");
+
+        return 0;
     }
 
-    private static string GetFileName(DateOnly date, string title) => $"{date:yyyy-MM-dd}-{ToKebabCase(title)}.md";
+    private static IReadOnlyDictionary<string, object> CreateFrontMatterHeader(CreatePostSettings settings, ILookup<string, string?> remaining)
+    {
+        var frontMatter = new Dictionary<string, object>
+        {
+            ["title"] = settings.Title,
+            ["date"] = settings.PostDate.ToDateTime(settings.PostTime).ToString("yyyy-MM-dd hh:mm:ss"),
+            ["layout"] = settings.Layout
+        };
 
-    private static string ToKebabCase(string fileName) => fileName
-        .ToLowerInvariant()
-        .Replace(":", "")
-        .Replace(",", "")
-        .Replace(" ", "-");
+        if (!string.IsNullOrWhiteSpace(settings.Excerpt))
+        {
+            frontMatter.Add("excerpt", settings.Excerpt);
+        }
+
+        foreach (var item in remaining)
+        {
+            switch (item.Count())
+            {
+                case > 1:
+                    frontMatter.Add(item.Key, item);
+
+                    break;
+
+                case 1 when item.Single() is { } value:
+                    frontMatter.Add(item.Key, value);
+
+                    break;
+            }
+        }
+
+        return frontMatter;
+    }
+
+    private static string GetFileName(CreatePostSettings settings)
+    {
+        var result = new StringBuilder();
+
+        result.Append(settings.PostDate.ToString("yyyy-MM-dd"));
+        
+        result.Append('-');
+
+        result.Append(string.IsNullOrEmpty(settings.FileName) ? ToKebabCase(settings.Title) : ToKebabCase(settings.FileName));
+
+        result.Append(".md");
+
+        return result.ToString();
+    }
+
+    private static string ToKebabCase(string value)
+    {
+        // Replace all non-alphanumeric characters with a dash
+        value = Regex.Replace(value, @"[^0-9a-zA-Z]", "-");
+
+        // Replace all subsequent dashes with a single dash
+        value = Regex.Replace(value, @"[-]{2,}", "-");
+
+        // Remove any trailing dashes
+        value = Regex.Replace(value, @"-+$", string.Empty);
+
+        // Remove any dashes in position zero
+        if (value.StartsWith("-")) value = value[1..];
+
+        // Lowercase and return
+        return value.ToLower();
+    }
 }
--- src/Hyde/Program.cs
+++ src/Hyde/Program.cs
@@ -3,8 +3,14 @@ using Hyde.Commands.Post;
 using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console.Cli;
 using Spectre.Console.Cli.Extensions.DependencyInjection;
+using YamlDotNet.Serialization;
 
 var services = new ServiceCollection();
+
+services.AddSingleton(_ => new SerializerBuilder()
+    .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull | DefaultValuesHandling.OmitEmptyCollections | DefaultValuesHandling.OmitDefaults));
+
+services.AddSingleton<ISerializer>(sp => sp.GetRequiredService<SerializerBuilder>().Build());
 
 var registrar = new DependencyInjectionRegistrar(services);
 
@@ -19,32 +25,32 @@ app.Configure(config =>
         post.AddCommand<CreatePostCommand>("create")
             .WithDescription("Creates a new post");
 
-        post.AddCommand<ListPostCommand>("list")
-            .WithDescription("Lists all available posts");
-        
-        post.AddCommand<ViewPostCommand>("view")
-            .WithDescription("View a post");
-        
-        post.AddCommand<DeletePostCommand>("delete")
-            .WithDescription("Deletes a posts");
+        // post.AddCommand<ListPostCommand>("list")
+        //     .WithDescription("Lists all available posts");
+        //
+        // post.AddCommand<ViewPostCommand>("view")
+        //     .WithDescription("View a post");
+        //
+        // post.AddCommand<DeletePostCommand>("delete")
+        //     .WithDescription("Deletes a posts");
     });
     
-    config.AddBranch("page", page =>
-    {
-        page.SetDescription("Manage pages");
-        
-        page.AddCommand<CreatePageCommand>("create")
-            .WithDescription("Creates a page");
-        
-        page.AddCommand<ListPageCommand>("list")
-            .WithDescription("Lists all available pages");
-        
-        page.AddCommand<ViewPageCommand>("view")
-            .WithDescription("View a page");
-        
-        page.AddCommand<DeletePageCommand>("delete")
-            .WithDescription("Deletes a page");
-    });
+    // config.AddBranch("page", page =>
+    // {
+    //     page.SetDescription("Manage pages");
+    //     
+    //     page.AddCommand<CreatePageCommand>("create")
+    //         .WithDescription("Creates a page");
+    //     
+    //     page.AddCommand<ListPageCommand>("list")
+    //         .WithDescription("Lists all available pages");
+    //     
+    //     page.AddCommand<ViewPageCommand>("view")
+    //         .WithDescription("View a page");
+    //     
+    //     page.AddCommand<DeletePageCommand>("delete")
+    //         .WithDescription("Deletes a page");
+    // });
 });
 
 return app.Run(args);
